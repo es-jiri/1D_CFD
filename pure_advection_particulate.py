0b06f895eda8c37fa84e75d7e36a416b0e6e9628
jiri.strouhal <Jiri.Strouhal@vutbr.cz>
2022-06-21 14:23:31 +0200

    Before simplification

--- pure_advection_particulate.py
+++ pure_advection_particulate.py
@@ -1,7 +1,20 @@
-from ctypes import c_bool
-from mimetypes import init
+
 from scipy.linalg import solve
 from matplotlib import pyplot
+
+
+def show_balance(inflow=0,outflow=0,source=0,accumulation=0,init_amount=0):
+    print('init amount:', init_amount)
+    print('inflow:', inflow)
+    print('outflow:', outflow)
+    print('source:', source)
+    print('accumulation:', accumulation)
+    absolute_imbalance = outflow - inflow + accumulation
+    denom = abs(init_amount)+abs(inflow)
+    relative_imbalance = absolute_imbalance/denom if denom>0 else 1
+    print('absolute imbalance:', absolute_imbalance)
+    print('relative imbalance:', relative_imbalance)
+
 
 
 VELOCITY = 0.5
@@ -38,21 +51,15 @@ grad_m_B = 0
 
 
 rho_0 = [0.5]*N_NODES
-
 m_0 = [0.001]*N_NODES
 c_0 = [rho_0[i]/m_0[i] for i in range(len(m_0))]
 
 
-init_amount = sum([m_0[i]*c_0[i] for i in range(len(m_0))])*DELTA_X
-
-
 time = 0
-
-
-absolute_imbalance = 0
 inflow = 0
 outflow = 0
 accumulation = 0
+init_amount = sum([m_0[i]*c_0[i] for i in range(len(m_0))])*DELTA_X
 
 
 vals = [c_A*m_A]
@@ -64,7 +71,7 @@ plot_time = 0
 
 while(time<TIME):
 
-    #particle concentration
+    # =========================== particle concentration
     ac_P0 = 1/DELTA_T
     ac_P = ac_P0 + VELOCITY*(2*eps(VELOCITY)-1)/DELTA_X
     ac_E = -VELOCITY*(1-eps(VELOCITY))/DELTA_X
@@ -104,34 +111,35 @@ while(time<TIME):
     if VELOCITY>0: c_B = c[-1]+0.5*DELTA_X*grad_c_B
     else: c_A = c[0]-0.5*DELTA_X*grad_c_A
 
-
-    #particle mass
+    # =========================== particle mass
     am_P = [0]*N_NODES
     am_P0 = [0]*N_NODES
     am_E = [0]*N_NODES
     am_W = [0]*N_NODES
 
-    c_face = [c_A] + [(c[i]+c[i+1])*0.5 for i in range(N_NODES-1)] + [c_B]
+    cc = [c_A]
+    [cc.append(c_i) for c_i in c]
+    cc.append(c_B)
 
-    C_P_MIN = 1
- 
+    C_P_MIN = 1000
+    
     for i in range(N_NODES):
-        c_P = max(c[i], C_P_MIN)
+        c_P = max(cc[i+1], C_P_MIN)
         c_P_0 = max(c_0[i], C_P_MIN)
         am_P0[i] = c_P_0/DELTA_T
-        am_P[i] = c_P/DELTA_T + VELOCITY/DELTA_X*(eps(VELOCITY)*c_face[i]-(eps(VELOCITY)-1)*c_face[i+1])
-        am_E[i] = -VELOCITY/DELTA_X * (1-eps(VELOCITY))*c_face[i+1]
-        am_W[i] = VELOCITY/DELTA_X * eps(VELOCITY)*c_face[i]
+        am_P[i] = c_P/DELTA_T + VELOCITY/DELTA_X*(eps(VELOCITY)*cc[i]-(eps(VELOCITY)-1)*cc[i+2])
+        am_E[i] = -VELOCITY/DELTA_X * (1-eps(VELOCITY))*cc[i+2]
+        am_W[i] = VELOCITY/DELTA_X * eps(VELOCITY) * cc[i]
 
     am_E_A = am_E[0]
     am_W_B = am_W[-1]
-    if VELOCITY>0:
+    if VELOCITY>0:  
         am_P_A = am_P[0]
-        am_P_B = am_P0[-1] + VELOCITY/DELTA_X*eps(VELOCITY)*c_B
+        am_P_B = am_P0[-1] + VELOCITY/DELTA_X*eps(VELOCITY)*cc[-1]
         Sm_C_A = VELOCITY/DELTA_X*eps(VELOCITY)*m_A*c_A
         Sm_C_B = -0.5*VELOCITY*(1-eps(VELOCITY))*grad_m_B*c_B
     else:
-        am_P_A = am_P0[0] + VELOCITY/DELTA_X*(eps(VELOCITY)-1)*c_A
+        am_P_A = am_P0[0] + VELOCITY/DELTA_X*(eps(VELOCITY)-1)*cc[0]
         am_P_B = am_P[-1]
         Sm_C_A = 0.5*VELOCITY*eps(VELOCITY)*grad_m_A*c_A
         Sm_C_B = -VELOCITY/DELTA_X*(1-eps(VELOCITY))*m_B*c_B
@@ -173,19 +181,14 @@ while(time<TIME):
     inflow += VELOCITY*DELTA_T*(c_A*m_A if VELOCITY>0 else -c_B*m_B)
     outflow += VELOCITY*DELTA_T*(c_B*m_B if VELOCITY>0 else -c_A*m_A)
     accumulation += DELTA_X*sum([c[i]*m[i]-c_0[i]*m_0[i] for i in range(len(m))])
-    absolute_imbalance = outflow - inflow + accumulation
-    relative_imbalance = absolute_imbalance/(abs(init_amount)+abs(inflow))
     
     c_0 = c
     m_0 = m
     time += DELTA_T
 
-print('init amount:', init_amount)
-print('inflow:', inflow)
-print('outflow:', outflow)
-print('accumulation:', accumulation)
-print('absolute imbalance:', absolute_imbalance)
-print('relative imbalance:', relative_imbalance)
+
+show_balance(inflow=inflow, outflow=outflow, accumulation=accumulation, init_amount=init_amount)
+
 
 
 pyplot.show()
